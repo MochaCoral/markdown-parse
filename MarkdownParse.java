907ccc751adabbcb12d70eb29cbcb128373def31
MochaCoral <90062849+MochaCoral@users.noreply.github.com>
2022-02-03 10:52:57 -0800

    switched paste location

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,50 +1,40 @@
-import static org.junit.Assert.*;
-
-import java.io.File;
+// File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
+import java.util.ArrayList;
 
-import org.junit.*;
+public class MarkdownParse {
+    public static ArrayList<String> getLinks(String markdown) throws IOException {
+        ArrayList<String> toReturn = new ArrayList<>();
+        // find the next [, then find the ], then find the (, then take up to
+        // the next )
+        int currentIndex = 0;
+        while(currentIndex < markdown.length()) {
 
-public class MarkdownParseTest {
-    @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
-    }
+            int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+            int openParen = markdown.indexOf("(", nextCloseBracket);
+            int closeParen = markdown.indexOf(")", openParen);
 
-    @Test
-    public void testGetLinks() throws IOException {
-        Path filename = Path.of("test-file2.md");
-        String contents = Files.readString(filename);
-        assertEquals(MarkdownParse.getLinks(contents), List.of("https://something.com", "some-page.html"));
+            //checks for infinite looping
+            if (nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) {
+                break;
+            }
 
-        filename = Path.of("test-file.md");
-        contents = Files.readString(filename);
-        assertEquals(MarkdownParse.getLinks(contents), List.of("https://something.com", "some-page.html", "hello"));
-
-        filename = Path.of("test-file3.md");
-        contents = Files.readString(filename);
-        assertEquals(MarkdownParse.getLinks(contents), List.of());
-
-        filename = Path.of("test-file4.md");
-        contents = Files.readString(filename);
-        assertEquals(MarkdownParse.getLinks(contents), List.of("a link on the first line"));
-    }
-
-    /*
-    @Test
-    public void testGetLinks() {
-        boolean exceptionThrown = false;
-        Path filename = Path.of("test-file2.md");
-        String contents;
-        try {
-            contents = Files.readString(filename);
-        } catch (IOException e) {
-            exceptionThrown = true;
+            //checks for brackets and parentheses with stuff between them, and empty links
+            if (nextCloseBracket + 1 == openParen && openParen + 1 != closeParen) {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
+            
+            currentIndex = closeParen + 1;
         }
-        assertEquals(false, exceptionThrown);
+        return toReturn;
     }
-    */
+    public static void main(String[] args) throws IOException {
+		Path fileName = Path.of(args[0]);
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = getLinks(contents);
+        System.out.println(links);
+    }
 }
